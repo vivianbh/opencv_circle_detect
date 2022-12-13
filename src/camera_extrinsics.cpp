7333eedf69fc68b3217a75315f91391bd6d8d1ce
vivianbh <hadallir@gmail.com>
2022-12-13 08:41:26 +0000

    stable

--- src/camera_extrinsics.cpp
+++ src/camera_extrinsics.cpp
@@ -1,21 +1,30 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h" 
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/PoseStamped.h"
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Core>
 #include <cmath>
 
-geometry_msgs::Twist position; // store object's coordinate
+geometry_msgs::Point position; // store object's coordinate
 Eigen::Vector3d pos;
 Eigen::Vector3d trans_pos;
+Eigen::Vector3d mav;
+geometry_msgs::Point mav_pose;
 
-// R: [x-axis 90 degrees -> z-axis 90 degrees]
+void getMAV(const geometry_msgs::PoseStamped::ConstPtr& pose) {
+    mav_pose.x = pose->pose.position.x;
+    mav_pose.y = pose->pose.position.y;
+    mav_pose.z = pose->pose.position.z;
+}
+
+// R: [x-axis 180]
 // T: [x, y, z] = [] (measured)
 void frameTransfer(const geometry_msgs::Point::ConstPtr& point) {
 	Eigen::Matrix3d R1, R2;
-        Eigen::Vector3d T(0.135, 0.485, 0.360);
-        float theta_x = 90*M_PI/180;
+        Eigen::Vector3d T(-0.04, 0.1, -0.1);
+        float theta_x = -180*M_PI/180;
         float theta_z = 90*M_PI/180;
         R1 << 1,             0,            0,
               0,  cos(theta_x), sin(theta_x),
@@ -25,25 +34,28 @@ void frameTransfer(const geometry_msgs::Point::ConstPtr& point) {
                          0,            0, 1;
 
 	pos << point -> x, point -> y, point -> z;
-        trans_pos = R2*R1*pos+T;
+    mav << mav_pose.x, mav_pose.y, mav_pose.z;
+        trans_pos = R1*pos+T+mav;
 
-	position.linear.x = trans_pos.x();
-	position.linear.y = trans_pos.y();
-	position.linear.z = trans_pos.z();
+	position.x = trans_pos.x();
+	position.y = trans_pos.y();
+	position.z = trans_pos.z();
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "camera_extrinsics");
 	ros::NodeHandle nh;
 	ros::Subscriber sub_cam2disk_pos;
+    ros::Subscriber sub_mav1_pos;
 	ros::Publisher pub_world2disk_pos;
 	
 	sub_cam2disk_pos = nh.subscribe("camera/disk_position", 30, frameTransfer);
-	ros::Rate r(100);
+    sub_mav1_pos = nh.subscribe("/vrpn_client_node/MAV1/pose", 30, getMAV);
+	ros::Rate r(30);
 
 	while(ros::ok())
 	{
-		pub_world2disk_pos = nh.advertise<geometry_msgs::Twist>("world/disk_position", 100);
+		pub_world2disk_pos = nh.advertise<geometry_msgs::Point>("world/disk_position", 100);
 		pub_world2disk_pos.publish(position);
 
 		ros::spinOnce();
--- src/circle_detector.py
+++ src/circle_detector.py
@@ -12,152 +12,156 @@ import cv2
 import time
 
 class Detect():
-	def __init__(self):
-		#################### node initialization & create topic ####################
-		rospy.init_node("circle_detector", anonymous=False)
-		self.coor_pub = rospy.Publisher("camera/disk_position", Point, queue_size= 30)
-		self.img_pub = rospy.Publisher("camera_image/detected", Image, queue_size=5)
-		self.flag = Int32()
-		self.data = Point()
-		self.img = Image()
-		self.color_image = np.asanyarray(0)
-		#################### video stream setting ####################
-		self.pipeline = rs.pipeline()
-		self.config = rs.config()
-		self.config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-		# Start streaming
-		self.pipeline.start(self.config)
-		self.time1 = time.time()
-		self.time2 = time.time()
-		#################### bounding object configuration ####################
-		self.lower_red = np.array([150,100,100])
-		self.upper_red = np.array([180,255,255])
-		self.contour_area = 0
-		self.kernel5 = np.ones((5, 5), np.uint8)
-		self.kernel9 = np.ones((9, 9), np.uint8)	
-		#################### camera's intrinsic parameter ####################
-		self.fx = 893.7527
-		self.fy = 895.8022
-		self.cx = 648.9854
-		self.cy = 374.8394
-		self.object_real_width = 0.08
-		self.intrinsic_matrix = np.array([[self.fx,       0, self.cx],
-						[      0, self.fy, self.cy],
-									      [      0,       0,       1]])
-		try :
-			self.inverse_intrinsic_matrix = np.linalg.inv(self.intrinsic_matrix)
-		except:
-			#sys.exit("intrinsic matrix doesn't have a inverse matrix.")
-			print("intrinsic matrix doesn't have a inverse matrix.")
+        def __init__(self):
+                #################### node initialization & create topic ####################
+                rospy.init_node("circle_detector", anonymous=False)
+                self.coor_pub = rospy.Publisher("camera/disk_position", Point, queue_size= 30)
+                self.img_pub = rospy.Publisher("camera_image/detected", Image, queue_size=5)
+                self.flag = Int32()
+                self.data = Point()
+                self.img = Image()
+                self.color_image = np.asanyarray(0)
+                #################### video stream setting ####################
+                self.pipeline = rs.pipeline()
+                self.config = rs.config()
+                self.config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+                # Start streaming
+                self.pipeline.start(self.config)
+                self.time1 = time.time()
+                self.time2 = time.time()
+                #################### bounding object configuration ####################
+                self.lower_red = np.array([150,100,100])
+                self.upper_red = np.array([180,255,255])
+                self.contour_area = 0
+                self.kernel5 = np.ones((5, 5), np.uint8)
+                self.kernel9 = np.ones((9, 9), np.uint8)        
+                #################### camera's intrinsic parameter ####################
+                self.fx = 893.7527
+                self.fy = 895.8022
+                self.cx = 648.9854
+                self.cy = 374.8394
+                self.object_real_width = 0.08
+                self.intrinsic_matrix = np.array([[self.fx,       0, self.cx],
+                                                [      0, self.fy, self.cy],
+                                                                              [      0,       0,       1]])
+                try :
+                        self.inverse_intrinsic_matrix = np.linalg.inv(self.intrinsic_matrix)
+                except:
+                        #sys.exit("intrinsic matrix doesn't have a inverse matrix.")
+                        print("intrinsic matrix doesn't have a inverse matrix.")
 
-	def find_contour(self):
-		hsv = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2HSV)
-		self.mask = cv2.inRange(hsv, self.lower_red, self.upper_red)
-		self.denoise_mask = cv2.morphologyEx(self.mask, cv2.MORPH_OPEN, self.kernel9)
-		self.denoise_mask2 = cv2.morphologyEx(self.denoise_mask, cv2.MORPH_CLOSE, self.kernel5, iterations = 3)
-		#self.res = cv2.bitwise_and(self.color_image, self.color_image, mask = self.denoise_mask2)
-		#self.gray = cv2.cvtColor(self.res, cv2.COLOR_BGR2GRAY)
-		#self.blur = cv2.GaussianBlur(self.gray, (5, 5), 0)
-		self.binary_img = cv2.Canny(self.denoise_mask2, 20, 160)
-		self.contours = cv2.findContours(self.binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]	
+        def find_contour(self):
+                hsv = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2HSV)
+                self.mask = cv2.inRange(hsv, self.lower_red, self.upper_red)
+                self.denoise_mask = cv2.morphologyEx(self.mask, cv2.MORPH_OPEN, self.kernel9)
+                self.denoise_mask2 = cv2.morphologyEx(self.denoise_mask, cv2.MORPH_CLOSE, self.kernel5, iterations = 3)
+                #self.res = cv2.bitwise_and(self.color_image, self.color_image, mask = self.denoise_mask2)
+                #self.gray = cv2.cvtColor(self.res, cv2.COLOR_BGR2GRAY)
+                #self.blur = cv2.GaussianBlur(self.gray, (5, 5), 0)
+                self.binary_img = cv2.Canny(self.denoise_mask2, 20, 160)
+                self.contours = cv2.findContours(self.binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]       
 
-	def bound_contour(self):
+        def bound_contour(self):
                 if self.contours:
-		        # find the biggest target object.
-		        for c in self.contours:
-			        area = cv2.contourArea(c)
-			        if area > self.contour_area:
-				        self.contour_area = area
-				        contour = c
+                        for c in self.contours:
+                                area = cv2.contourArea(c)
+                                if area > self.contour_area:
+                                        self.one_contour = c
+                                        self.contour_area = area
+                        (x, y, self.w, self.h) = cv2.boundingRect(self.one_contour)
+                        cv2.rectangle(self.color_image, (x,y), (x+self.w, y+self.h), (0, 255, 0), 2)
+                        self.x = x + self.w / 2
+                        self.y = y + self.h / 2
+                        cv2.circle(self.color_image, (int(self.x), int(self.y)), 8, (1, 277, 254), -1)
+                # zero the contour_area
+                self.contour_area = 0 
 
-		        # if target is found by camera, draw the position information on frame.
-			(x, y, self.w, self.h) = cv2.boundingRect(contour)
-			cv2.rectangle(self.color_image, (x,y), (x+self.w, y+self.h), (0, 255, 0), 2)
-			self.x = x + self.w / 2
-			self.y = y + self.h / 2
-			cv2.circle(self.color_image, (int(self.x), int(self.y)), 8, (1, 277, 254), -1)
-			#print("object's (x, y) in pixel = ({x}, {y})".format(x = self.x, y = self.y))	
+        def check_object(self):
+                if self.contours:
+                        return True
+                else:
+                        return False
 
-		# zero the contour_area
-		self.contour_area = 0	
+        def streaming(self):
+                frames = self.pipeline.wait_for_frames()
+                while frames == 0:
+                        frames = self.pipeline.wait_for_frames()
 
-	def check_object(self):
-		# check whether target is found, and check whether husky is ready at the specific site
-		if self.contours:
-			return True
-		else:
-			return False
-
-	def streaming(self):
-		frames = self.pipeline.wait_for_frames()
-		color_frame = frames.get_color_frame()
-		self.color_image = np.asanyarray(color_frame.get_data())
-	
-	def show_result(self):
+                color_frame = frames.get_color_frame()
+                self.color_image = np.asanyarray(color_frame.get_data())
+        
+        def show_result(self):
                 cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
                 cv2.imshow("RealSense", self.color_image)
-		#cv2.imshow("mask", self.denoise_mask)
-		#cv2.imshow("denoise_mask", self.denoise_mask2)
-		#cv2.imshow("canny", self.binary_img)
-		#cv2.imshow("blur", self.blur)
+                #cv2.imshow("mask", self.denoise_mask)
+                #cv2.imshow("denoise_mask", self.denoise_mask2)
+                #cv2.imshow("canny", self.binary_img)
+                #cv2.imshow("blur", self.blur)
 
-	def get_video_size(self):
-		#width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-		#height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-		print("width is %d, height is %d"%(width, height))
+        def get_video_size(self):
+                #width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+                #height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                print("width is %d, height is %d"%(width, height))
 
-	def object_camera_coordinate(self):
-		# calculate the camera coordinate by triangle similarity theorem
-		camera_coordinate_z = (self.fx / self.w) * self.object_real_width
-		camera_coordinate_x = (self.x - self.cx) * camera_coordinate_z / self.fx
-		camera_coordinate_y = (self.y - self.cy) * camera_coordinate_z / self.fy
+        def object_camera_coordinate(self):
+                # calculate the camera coordinate by triangle similarity theorem
+                camera_coordinate_z = (self.fx / self.w) * self.object_real_width
+                camera_coordinate_x = (self.x - self.cx) * camera_coordinate_z / self.fx
+                camera_coordinate_y = (self.y - self.cy) * camera_coordinate_z / self.fy
+                # record position information
+                data = Point()
+                data.x = camera_coordinate_x
+                data.y = camera_coordinate_y
+                data.z = camera_coordinate_z
+                print('data: %f  %f  %f', data.x, data.y, data.z)
+                return data
 
-		# record position information
-		data = Point()
-		data.x = camera_coordinate_x
-		data.y = camera_coordinate_y
-		data.z = camera_coordinate_z
-		return data
+        def object_detect(self):
+                self.find_contour()
+                print('object detect check obj:  %d', d.check_object())
+                if self.check_object:
+                        self.bound_contour()
 
-	def object_detect(self):
-		self.find_contour()
-		self.bound_contour()
+        def image_publisher(self):
+                gray_img = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2GRAY)
+                header = Header(stamp = rospy.Time.now())
+                header.frame_id = "object"
+                self.img.header = header
+                self.img.height = 720
+                self.img.width = 1280
+                self.img.encoding = "mono8" #"bgr8"#"mono8"
+                self.img.step = 320*3
+                self.img.data = np.array(gray_img).tobytes()
+                self.img_pub.publish(self.img)
 
-	def image_publisher(self):
-		header = Header(stamp = rospy.Time.now())
-		header.frame_id = "object"
-		self.img.header = header
-		self.img.height = 720
-		self.img.width = 1280
-		self.img.encoding = "bgr8"
-		self.img.step = 180*3
-		self.img.data = np.array(self.color_image).tostring()
-		self.img_pub.publish(self.img)
+        def coordinate_publisher(self):
+                print('pub data')
+                self.data = self.object_camera_coordinate()
+                self.coor_pub.publish(self.data)
 
-	def coordinate_publisher(self):
-		self.data = self.object_camera_coordinate()
-		self.coor_pub.publish(self.data)
-
-	def FPS_estimator(self):
-		self.time1 = self.time2
-		self.time2 = time.time()
-		duration = self.time2 - self.time1
-		print("FPS : %d" %(1 / duration))
-
-	def end(self):
-		self.pipeline.stop()
+        def FPS_estimator(self):
+                self.time1 = self.time2
+                self.time2 = time.time()
+                duration = self.time2 - self.time1
+                print("FPS : %d" %(1 / duration))
+        def rate(self):
+                self.rate.sleep()
+                
+        def end(self):
+                self.pipeline.stop()
 
 if __name__ == "__main__":
-	d = Detect()
-	while not rospy.is_shutdown():
+        d = Detect()
+        rate = rospy.Rate(15)
+        while not rospy.is_shutdown():
                 d.streaming()
-		d.object_detect()
-                d.show_result()
-		d.image_publisher()
-		if d.check_object():
-			d.coordinate_publisher()
-		#d.get_video_size()
-		#d.FPS_estimator()
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break	
-	d.end()
+                d.object_detect()
+                d.image_publisher()
+                print('check obj:  %d', d.check_object())
+                if d.check_object():
+                        d.coordinate_publisher()
+                d.FPS_estimator()
+                rate.sleep()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break   
+        d.end()
